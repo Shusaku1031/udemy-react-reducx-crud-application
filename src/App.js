37611e621c36822fba6bf69c55e83b9dc55ac8fb
Shusaku1031 <tomita.3e23@gmail.com>
2020-08-14 01:21:00 +0900

    create class component and function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {Component}from 'react';
 
-function App() {
-  return (
-    <div>
-	  <h1>Hello,world!</h1>
-    </div>
-  );
+//function App() {
+// return (
+//    <React.Fragment>
+//      <label htmlFor="bar">bar</label>
+//      <input type="text" onChange={() => {console.log("I am clicked!")}} />
+//    </React.Fragment>
+// );
+//}
+
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+}
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
