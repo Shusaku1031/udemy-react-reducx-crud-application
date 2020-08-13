4defdab5ac528023eb5706d945863f4732cf61c1
Shusaku1031 <tomita.3e23@gmail.com>
2020-08-14 01:38:25 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component}from 'react';
+import React from 'react';
 
 //function App() {
 // return (
@@ -10,17 +10,28 @@ import React, {Component}from 'react';
 //}
 
 const App = () => {
-  return <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
+  const profiles = [
+    {name : "Taro", age : 10},
+    {name : "Hanako", age : 5},
+    {name : "Noname"}
+  ]
+  return (
+  <div>
+    {
+      profiles.map((profile) => {
+        return <User name={profile.name} age={profile.age} />
+      })
+    }
   </div>
+  )
 }
 
-const Cat = () => {
-  return <div>Meow</div>
+const User = (props) => {
+return <div>Hi, I am {props.name}!, and {props.age} years old</div>
+}
+
+User.defaultProps = {
+  age : 1
 }
 
 export default App;
