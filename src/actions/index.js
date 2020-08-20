7bd7d2ed2712d758aa4b80e4161e64a6e42e99eb
Shusaku1031 <tomita.3e23@gmail.com>
2020-08-20 17:21:08 +0900

    Reimplement action and action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DECREMENT"
+export const increment = () => ({
+        type: INCREMENT   
+})
 
-export const increment = () => {
-        type: "INCREMENT"   
-}
-
-export const decrement = () => {
-        type: "DECREMENT"
-}
+export const decrement = () => ({
+        type: DECREMENT
+})
